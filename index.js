4a4d89d409a8ab17b59afefaacef9116aaab450a
Mohamed Safouan Besrour <besrour_2010@live.com>
2022-07-11 16:09:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,41 @@ class ZodiacSign {
 			en : ['Aries: The Ram','Taurus: The Bull','Gemini: The Twins','Cancer: The Crab','Leo: The Lion','Virgo: The Virgin','Libra: The Scales','Scorpio: The Scorpion','Sagittarius: The Archer','Capricorn: The Goat','Aquarius: The Water Bearer','Pisces: The Fish'],			
 			fr : ['Bélier', 'Taureau', 'Gémeaux', 'Cancer', 'Lion', 'Vierge', 'Balance', 'Scorpion', 'Sagittaire', 'Capricorne', 'Vereau', 'Poissons'],
 			es : ['Aries', 'Tauro', 'Géminis', 'Cáncer', 'Leo', 'Virgo', 'Libra', 'Escorpio', 'Sagitario', 'Capricornio', 'Acuario', 'Piscis'],
-			ar : ['الحمل', 'الثور', 'الجوزاء', 'السرطان', 'الأسد', 'العذراء', 'الميزان', 'العقرب',' القوس', 'الجدي', 'الدلو', 'الحوت'],
-			ru : ['Овен', 'Телец', 'Близнецы', 'Рак', 'Лев', 'Дева', 'Весы', 'Скорпион', 'Змееносец', 'Стрелец', 'Козерог', 'Водолей', 'Рыбы'],
-			zh : ['白羊宮', '金牛宮', '雙子宮', '巨蟹宮', '獅子宮', '室女宮', '天秤宮', '天蝎宫', '人马宫', '摩羯宮', '寶瓶宮', '雙魚宮']
+			ar : ['الحمل', 'الثور', 'الجوزاء', 'السرطان', 'الأسد', 'العذراء', 'الميزان', 'العقرب',' القوس', 'الجدي', 'الدلو', 'الحوت']
+		}
+		
+		static chineseSigns = {
+			en : ['Monkey', 'Rooster', 'Dog', 'Pig', 'Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep'],
+			fr : ['Singe', 'Coq', 'Chien', 'Cochon', 'Rat', 'Bœuf', 'Tigre', 'Lapin', 'Dragon', 'Serpent', 'Cheval', 'Mouton'],
+			es : ['Mono', 'Gallo', 'Perro', 'Cerdo', 'Rata', 'Buey', 'Tigre', 'Conejo', 'Dragón', 'Serpiente', 'Caballo', 'Oveja'],
+			ar : ['القرد', 'الديك', 'الكلب', 'الخنزير', 'الفأر', 'الثور', 'النمر', 'الأرنب', 'التنين', 'الأفعى', 'الحصان', 'الخروف']
+		}
+		
+		static chineseElements = {
+			en : ['Metal', 'Water', 'Wood', 'Fire', 'Earth'],
+			fr : ['Métal', 'Eau', 'Bois', 'Feu', 'Terre'],
+			es : ['Metal', 'Agua', 'Madera', 'Fuego', 'Tierra'],
+			ar : ['المعدني', 'المائي', 'الخشبي', 'الناري', 'الأرضي']
 		}
 		
 		constructor(value, lang = 'en') {
-			this.value = value
-			this.lang = lang
-			this.sign = this.#getSign(this.value)
+			this.sign = ''
+			this.chinese = ''
+		
+			if (!Object.hasOwn(ZodiacSign.signs, lang)) lang = 'en'
+			if (!isNaN(Date.parse(value))){
+				this.sign = this.#getSign(value, lang)
+				this.chinese = this.#getChineseSign(value, lang)
+			}
 		}
 		
-		#getSign(x) {
-			if (isNaN(Date.parse(x))) return ''
-			else return ZodiacSign.signs[this.lang][Number(new Intl.DateTimeFormat('fr-TN-u-ca-persian', {month: 'numeric'}).format(Date.parse(x))) - 1];
+		#getSign(x, y) {
+			return ZodiacSign.signs[y][Number(new Intl.DateTimeFormat('fr-TN-u-ca-persian', {month: 'numeric'}).format(Date.parse(x))) - 1];
+		}
+		
+		#getChineseSign(x, y){
+			let chineseDate = new Intl.DateTimeFormat('fr-TN-u-ca-chinese', {day: '2-digit', month: 'long', year:'numeric'}).format(Date.parse(x)).substring(0, 4)
+			return `${ZodiacSign.chineseElements[y][Math.floor(+chineseDate.charAt(3) / 2)]} ${ZodiacSign.chineseSigns[y][+chineseDate % 12]}`
 		}
 		
 }
