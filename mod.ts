eb84a668f506daad2f61a24d0321323ea24f08f8
Mohamed Safouan Besrour <besrour_2010@live.com>
2024-03-01 15:24:51 +0100

    add symbol documentation

--- mod.ts
+++ mod.ts
@@ -1,7 +1,14 @@
+/**
+ * Represents a Zodiac sign with associated information.
+ */
 export class ZodiacSign {
     private sign: string;
     private chinese: string;
-
+    /**
+     * Signs for different languages.
+     * @example
+     * const signs = ZodiacSign.signs['en'];
+     */
     static signs: { [key: string]: string[] } = {
         en: ['Aries: The Ram', 'Taurus: The Bull', 'Gemini: The Twins', 'Cancer: The Crab', 'Leo: The Lion', 'Virgo: The Virgin', 'Libra: The Scales', 'Scorpio: The Scorpion', 'Sagittarius: The Archer', 'Capricorn: The Goat', 'Aquarius: The Water Bearer', 'Pisces: The Fish'],
         fr: ['Bélier', 'Taureau', 'Gémeaux', 'Cancer', 'Lion', 'Vierge', 'Balance', 'Scorpion', 'Sagittaire', 'Capricorne', 'Vereau', 'Poissons'],
@@ -13,7 +20,9 @@ export class ZodiacSign {
         ja: ['牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座', '山羊座', '水瓶座', '魚座'],
         id: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagitarius', 'Capricorn', 'Aquarius', 'Pisces']
     };
-
+    /**
+     * Chinese zodiac signs for different languages.
+     */
     static chineseSigns: { [key: string]: string[] } = {
         en: ['Monkey', 'Rooster', 'Dog', 'Pig', 'Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep'],
         fr: ['Singe', 'Coq', 'Chien', 'Cochon', 'Rat', 'Bœuf', 'Tigre', 'Lapin', 'Dragon', 'Serpent', 'Cheval', 'Mouton'],
@@ -25,7 +34,9 @@ export class ZodiacSign {
         ja: ['猿', '鶏', '犬', '猪', 'ネズミ', '牛', '虎', '兎', '龍', '蛇', '馬', '羊'],
         id: ['Monyet', 'Ayam', 'Anjing', 'Babi', 'Tikus', 'Sapi', 'Harimau', 'Kelinci', 'Naga', 'Ular', 'Kuda', 'Kambing']
     };
-
+    /**
+     * Chinese elements for different languages.
+     */
     static chineseElements: { [key: string]: string[] } = {
         en: ['Metal', 'Water', 'Wood', 'Fire', 'Earth'],
         fr: ['Métal', 'Eau', 'Bois', 'Feu', 'Terre'],
@@ -37,7 +48,9 @@ export class ZodiacSign {
         ja: ['金', '水', '木', '火', '土'],
         id: ['Logam', 'Air', 'Kayu', 'Api', 'Tanah']
     };
-
+    /**
+     * Yin and Yang for different languages.
+     */
     static yinYang: { [key: string]: string[] } = {
         en: ['Yang', 'Yin'],
         fr: ['Yang', 'Yin'],
@@ -49,7 +62,11 @@ export class ZodiacSign {
         ja: ['陽', '陰'],
         id: ['Yang', 'Yin']
     };
-
+    /**
+     * Creates an instance of the ZodiacSign class.
+     * @param value - The date string or timestamp.
+     * @param lang - The language code (default is 'en').
+     */
     constructor(value: string, lang: string = 'en') {
         this.sign = '';
         this.chinese = '';
@@ -60,11 +77,21 @@ export class ZodiacSign {
             this.chinese = this.getChineseSign(value, lang);
         }
     }
-
+    /**
+     * Gets the zodiac sign based on the provided date.
+     * @param x - The date string or timestamp.
+     * @param y - The language code.
+     * @returns The zodiac sign.
+     */
     private getSign(x: string, y: string): string {
         return ZodiacSign.signs[y][Number(new Intl.DateTimeFormat('fr-TN-u-ca-persian', { month: 'numeric' }).format(Date.parse(x))) - 1];
     }
-
+    /**
+     * Gets the Chinese zodiac sign, element, and Yin-Yang information based on the provided date.
+     * @param x - The date string or timestamp.
+     * @param y - The language code.
+     * @returns An object containing Chinese zodiac information.
+     */
     private getChineseSign(x: string, y: string): { sign: string; element: string; yinyang: string } {
         let chineseDate = new Intl.DateTimeFormat('fr-TN-u-ca-chinese', { day: '2-digit', month: 'long', year: 'numeric' }).format(Date.parse(x)).substring(0, 4);
 
